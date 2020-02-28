6d50dfccb1917b61d442cca4c3d1fe4bc98be199
Valenacheg <valenacheg@mail.ru>
2020-02-28 17:23:28 +0500

    deleting spi4ki

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -5,26 +5,23 @@ import Message from "./Message/Message";
 import TextArea from "./Textarea/TextArea";
 
 const Dialogs = props => {
-  let kek = 3;
-  let spi4ki = 4;
   let dialogsElements = props.dialogsPage.dialogs.map(d => (
-    
-
     <DialogItem name={d.name} id={d.id} avatar={d.avatar} />
   ));
 
   let messagesElements = props.dialogsPage.messages.map(m => (
-    <Message message={m.message} id={m.id}/>
+    <Message message={m.message} id={m.id} />
   ));
 
   return (
     <div className={s.dialogs}>
-      
       <div className={s.dialogsItems}>{dialogsElements}</div>
-      
+
       <div className={s.messages}>{messagesElements}</div>
-      <TextArea dispatch={props.dispatch} newMessageText={props.dialogsPage.newMessageText}/>
-      
+      <TextArea
+        dispatch={props.dispatch}
+        newMessageText={props.dialogsPage.newMessageText}
+      />
     </div>
   );
 };
