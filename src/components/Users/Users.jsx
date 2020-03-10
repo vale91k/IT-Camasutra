efc83f8daea4a09be9317586d6e6b7f84cc262c5
Valenacheg <valenacheg@mail.ru>
2020-03-10 20:00:54 +0500

    add axios responds users

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,66 +1,16 @@
 import React from "react";
 import styles from "./Users.module.css";
+import * as Axios from "axios";
+import userPhoto from '../../assets/images/user.jpg'
+
+
 
 const Users = props => {
   if (props.users.length === 0) {
-  
-    props.setUsers([
-      {
-        id: 1,
-        followed: true,
-        fullName: "Valentin",
-        status: "I am a pokerman",
-        location: { city: "Magnitogorsk", country: "Russia" },
-        avatar:
-          "https://cdn.pixabay.com/photo/2015/11/16/14/43/cat-1045782__340.jpg"
-      },
-      {
-        id: 2,
-        followed: true,
-        fullName: "Stanislav",
-        status: "I am a cocorouch",
-        location: { city: "Sibai", country: "Russia" },
-        avatar:
-          "https://imgix.bustle.com/uploads/getty/2019/11/20/e2ed92cb-3320-47cc-9ca3-bc0c3c5c615e-getty-1188506982.jpg?w=1020&h=574&fit=crop&crop=faces&auto=format&q=70"
-      },
-      {
-        id: 3,
-        followed: true,
-        fullName: "Peter",
-        status: "I am a showman!",
-        location: { city: "St.Piterburg", country: "Russia" },
-        avatar:
-          "https://cdn.pixabay.com/photo/2015/11/16/14/43/cat-1045782__340.jpg"
-      },
-      {
-        id: 4,
-        followed: false,
-        fullName: "Eugene",
-        status: "I am a businessman",
-        location: { city: "Paris", country: "France" },
-        avatar:
-          "https://media14.s-nbcnews.com/j/MSNBC/Components/Video/201905/tdy_news_grumpy_cat_190517_1920x1080.focal-760x428.jpg"
-      },
-      {
-        id: 5,
-        followed: false,
-        fullName: "Edward",
-        status: "O_o",
-        location: { city: "Magnitogorsk", country: "Russia" },
-        avatar:
-          "https://www.thehappycatsite.com/wp-content/uploads/2017/12/flat-faced-cats1.jpg"
-      },
-      {
-        id: 6,
-        followed: false,
-        fullName: "Ruslan",
-        status: "L000l",
-        location: { city: "Magnitogorsk", country: "Russia" },
-        avatar:
-          "https://www.womansworld.com/wp-content/uploads/2019/06/cat-with-flat-face.jpg?w=1024"
-      }
-    ]
-    )
+  Axios.get("https://social-network.samuraijs.com/api/1.0/users").then(Response => {
+  props.setUsers(Response.data.items)
+})
+    
   }
   
   return (
@@ -69,7 +19,7 @@ const Users = props => {
         <div key={x.id} className={styles.userBar}>
           <span className={styles.leftPart}>
             <div >
-              <img className={styles.avatar} src={x.avatar} />
+              <img className={styles.avatar} src={x.photos.small != null ? x.photos.small : userPhoto} />
             </div>
             <div >
               {x.followed ? (
@@ -90,15 +40,16 @@ const Users = props => {
                 </button>
               )}
             </div>
+            {/* ! хардкодим 1 - аватарку, 2 - город, 3 - страну */}
           </span>
           <span className={styles.rightBar}>
             <span className={styles.leftSideBar}>
-              <div className={styles.fullName}>{x.fullName}</div>
+              <div className={styles.fullName}>{x.name}</div>
               <div className={styles.status}>{x.status}</div>
             </span>
             <span className={styles.rightsideBar}>
-              <div className={styles.city}>{x.location.city}</div>
-              <div className={styles.country}>{x.location.country}</div>
+              <div className={styles.city}>mgn</div>
+              <div className={styles.country}>russia</div>
             </span>
           </span>
         </div>
