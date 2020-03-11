264b4aa9aa6a21d16470956303461202b1ceeef7
Valenacheg <valenacheg@mail.ru>
2020-03-11 18:07:50 +0500

    53 Refactor to class Component Users

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -3,58 +3,69 @@ import styles from "./Users.module.css";
 import * as Axios from "axios";
 import userPhoto from '../../assets/images/user.jpg'
 
+class Users extends React.Component {
 
+constructor(props) {
+super(props);
 
-const Users = props => {
-  if (props.users.length === 0) {
+alert('new')
   Axios.get("https://social-network.samuraijs.com/api/1.0/users").then(Response => {
-  props.setUsers(Response.data.items)
+  this.props.setUsers(Response.data.items)
 })
     
-  }
   
-  return (
-    <div>
-      {props.users.map(x => (
-        <div key={x.id} className={styles.userBar}>
-          <span className={styles.leftPart}>
-            <div >
-              <img className={styles.avatar} src={x.photos.small != null ? x.photos.small : userPhoto} />
-            </div>
-            <div >
-              {x.followed ? (
-                <button
-                  onClick={ () => {
-                    props.unfollow(x.id);
-                  }}
-                >
-                  Follow
-                </button>
-              ) : (
-                <button
-                  onClick={() => {
-                    props.follow(x.id);
-                  }}
-                >
-                  UnFollow
-                </button>
-              )}
-            </div>
-            {/* ! хардкодим 1 - аватарку, 2 - город, 3 - страну */}
-          </span>
-          <span className={styles.rightBar}>
-            <span className={styles.leftSideBar}>
-              <div className={styles.fullName}>{x.name}</div>
-              <div className={styles.status}>{x.status}</div>
+}
+
+   getUsers = () => {
+
+    
+  }
+  render() {
+    return (
+      <div>
+        <button onClick={this.getUsers}>users?</button>
+        {this.props.users.map(x => (
+          <div key={x.id} className={styles.userBar}>
+            <span className={styles.leftPart}>
+              <div >
+                <img className={styles.avatar} src={x.photos.small != null ? x.photos.small : userPhoto} />
+              </div>
+              <div >
+                {x.followed ? (
+                  <button
+                    onClick={ () => {
+                      this.props.unfollow(x.id);
+                    }}
+                  >
+                    Follow
+                  </button>
+                ) : (
+                  <button
+                    onClick={() => {
+                      this.props.follow(x.id);
+                    }}
+                  >
+                    UnFollow
+                  </button>
+                )}
+              </div>
+              {/* ! хардкодим 1 - аватарку, 2 - город, 3 - страну */}
             </span>
-            <span className={styles.rightsideBar}>
-              <div className={styles.city}>mgn</div>
-              <div className={styles.country}>russia</div>
+            <span className={styles.rightBar}>
+              <span className={styles.leftSideBar}>
+                <div className={styles.fullName}>{x.name}</div>
+                <div className={styles.status}>{x.status}</div>
+              </span>
+              <span className={styles.rightsideBar}>
+                <div className={styles.city}>mgn</div>
+                <div className={styles.country}>russia</div>
+              </span>
             </span>
-          </span>
-        </div>
-      ))}
-    </div>
-  );
-};
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
+
 export default Users;
