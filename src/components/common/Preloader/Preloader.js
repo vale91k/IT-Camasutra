ca93e18144cd4919cd94e196007d07e1c926b076
Valenacheg <valenacheg@mail.ru>
2020-03-26 23:52:16 +0500

    change preloader gif

--- src/components/common/Preloader/Preloader.js
+++ src/components/common/Preloader/Preloader.js
@@ -1,9 +1,9 @@
 import React from 'react'
-
+import * as pic from './../../common/Preloader/Header_Preloader.gif'
 let Preloader =(props) => {
  
     
-return (<div><img src='https://i.gifer.com/CK1.gif'/></div>)
+return (<div><img src={pic} alt='https://i.gifer.com/CK1.gif'/></div>)
 }
 
 export default Preloader
