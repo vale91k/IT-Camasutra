1ed7c9878d0c0732bef2af033bd291cec378a0d6
Valenacheg <valenacheg@mail.ru>
2020-04-04 00:24:52 +0500

    +add Test mission 1

--- src/components/Test/Test.jsx
+++ src/components/Test/Test.jsx
@@ -1,22 +1,41 @@
 import React from "react";
-import ss from "./Test.module.css";
-import ButtonsWithFollow from "./ButtonsWithFollow/ButtonsWithFollow";
+import styles from "./Test.module.css";
 
-const Test = props => {
-  return (
-    <div>
-      <ButtonsWithFollow
-        followedAC={props.followedAC}
-        isFollowed={props.isFollowed}
-        isLoading={props.isLoading}
-        isLoadingAC={props.isLoadingAC}
-        test={props.test}
-      />
-      <input autoFocus={true} type="text" />
+class Test extends React.Component {
+  state = {
+    count: 0
+  };
+  incButton = () => {
+    if (this.state.count < 5) {
+      this.setState({
+        count: this.state.count + 1
+      });
+    }
+  };
+  resetOnClick = () => {
+    this.setState({ count: 0 });
+  };
 
-      <input autoFocus={true} value="tex2t" />
-      <input autoFocus={true} value="text" />
-    </div>
-  );
-};
+  render() {
+    const isFive = this.state.count === 5;
+
+    return (
+      <div
+        className={styles.container + " " + (isFive ? styles.red : styles.blue)}
+      >
+        <span className={styles.count}>{this.state.count}</span>
+        <div className={styles.buttons}>
+          <div className={styles.button1}>
+            <button onClick={this.incButton}>Inc</button>
+          </div>
+          <div className={styles.button2}>
+            {" "}
+            <button onClick={this.resetOnClick}>Reset</button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
 export default Test;
--- src/components/Test/TestContainer.jsx
+++ src/components/Test/TestContainer.jsx
@@ -4,29 +4,21 @@ import { connect } from "react-redux";
 import { followedAC, isLoadingAC } from "../../redux/test-reducer";
 
 class TestContainer extends React.Component {
-  debugger;
+  
   render() {
     return (
       <div>
-        <Test
-          followedAC={this.props.followedAC}
-          isFollowed={this.props.isFollowed}
-          isLoading={this.props.isLoading}
-          isLoadingAC={this.props.isLoadingAC}
-          test={this.props.test}
-        />
+        <Test />
       </div>
     );
   }
 }
 const MapStatetoProps = state => {
   return {
-    buttons: state.testPage.buttons,
-    isFollowed: state.testPage.isFollowed,
-    isLoading: state.testPage.isLoading,
-    test: state.testPage.test
+    
+    
   };
 };
-export default connect(MapStatetoProps, { followedAC, isLoadingAC })(
+export default connect(MapStatetoProps, { })(
   TestContainer
 );
