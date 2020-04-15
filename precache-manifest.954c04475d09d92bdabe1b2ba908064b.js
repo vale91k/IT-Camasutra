ecba5987cd6345fc7ab5946bfea293cde35cd05a
ValenachegBack <valenacheg@yandex.ru>
2020-04-15 22:16:59 +0500

    Updates

---
+++ precache-manifest.954c04475d09d92bdabe1b2ba908064b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04f8988c7f94a9d55082276515ab6bf2",
+    "url": "/IT-Camasutra/index.html"
+  },
+  {
+    "revision": "bcff33629e494e2e61fa",
+    "url": "/IT-Camasutra/static/css/3.880ea170.chunk.css"
+  },
+  {
+    "revision": "b763df694fcbdee4ad6a",
+    "url": "/IT-Camasutra/static/css/4.d33b190e.chunk.css"
+  },
+  {
+    "revision": "a9fc09c4682f677200f4",
+    "url": "/IT-Camasutra/static/css/main.d9cf3ee4.chunk.css"
+  },
+  {
+    "revision": "4f82e50e7c40824d1f96",
+    "url": "/IT-Camasutra/static/js/2.a4d547b2.chunk.js"
+  },
+  {
+    "revision": "811b3d2d36f5d93ac3b9bf1c6d65ac21",
+    "url": "/IT-Camasutra/static/js/2.a4d547b2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bcff33629e494e2e61fa",
+    "url": "/IT-Camasutra/static/js/3.2eba18d5.chunk.js"
+  },
+  {
+    "revision": "b763df694fcbdee4ad6a",
+    "url": "/IT-Camasutra/static/js/4.519e9763.chunk.js"
+  },
+  {
+    "revision": "a9fc09c4682f677200f4",
+    "url": "/IT-Camasutra/static/js/main.68ebe54e.chunk.js"
+  },
+  {
+    "revision": "7bd34978e29b15dce59b",
+    "url": "/IT-Camasutra/static/js/runtime-main.9759608c.js"
+  },
+  {
+    "revision": "b8adbec9d887b5b1c05df1b61c00ff93",
+    "url": "/IT-Camasutra/static/media/Header_Preloader.b8adbec9.gif"
+  },
+  {
+    "revision": "254ff478ac67ef5357d27d50a330ca7e",
+    "url": "/IT-Camasutra/static/media/f5baef4b6b6677020ab8d091ef78a3bc_w200.254ff478.gif"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/IT-Camasutra/precache-manifest.954c04475d09d92bdabe1b2ba908064b.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/IT-Camasutra/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
